aadcffb7c6b4ca2927ccfb849744dc15ed5f26aa
shawnbarron16 <shawnbarron16@gmail.com>
2023-08-08 18:04:31 -0500

    started work on layout of the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,43 @@
 <body class="gradient-background">
-<section class="primer">
-	<div>
-		<h2>
-			Elevate Your Brand, Expand Your Reach
-		</h2>
-		<p>
-			At Prime Product Partners, we provide the most creative strategies revolving around your Amazon store creation. 
-			We pride ourselves on working exclusively with small and local businesses. If you run a business or even just hand-make your own art or custom items, 
-			there's a place for you with us!
-		</p>
-	</div>
-</section>
-<section class="section-1">
-	<div>
-		<h2>
-			Unlock a New Revenue Stream
-		</h2>
-		<p>
-			Every Business Can Benefit from an Additional Revenue Stream, Especially Small Enterprises!
-		</p>
-	</div>
-	<img src="/images/revenue-img.png" alt="a hand holding a smartphone showing amazon sales.">
-</section>
-<section class="section-2">
-	<img src="/images/reach-img.png" alt="graph showing growth in sales.">
-	<div>
-		<h2>
-			Expand Your Reach
-		</h2>
-		<p>
-			Get noticed by thousands of new customers, all while keeping the same small business feel!
-		</p>
-	</div>
-</section>
-<section class="section-3">
-	<div>
-		<h2>
-			Use Amazon in Creative Ways
-		</h2>
-		<p>
-			Leverage Amazon to sell you hand made goods, publish books, and partner with other businesses to create unique product offerings.
-		</p>
-	</div>
-	<img src="/images/creative-img.png" alt="a collection of custom created books.">
-</section>
+	<section class="primer">
+		<div>
+			<h1>Elevate Your Brand, Expand Your Reach</h1>
+			<p>
+				At Prime Product Partners, we provide the most creative strategies revolving around your
+				Amazon store creation. We pride ourselves on working exclusively with small and local
+				businesses. If you run a business or even just hand-make your own art or custom items,
+				there's a place for you with us!
+			</p>
+		</div>
+	</section>
+	<section class="section-1">
+		<div>
+			<h2>Unlock a New Revenue Stream</h2>
+			<p>
+				Every Business Can Benefit from an Additional Revenue Stream, Especially Small Enterprises!
+			</p>
+		</div>
+		<img src="/images/revenue-img.png" alt="a hand holding a smartphone showing amazon sales." />
+	</section>
+	<section class="section-2">
+		<img src="/images/reach-img.png" alt="graph showing growth in sales." />
+		<div>
+			<h2>Expand Your Reach</h2>
+			<p>
+				Get noticed by thousands of new customers, all while keeping the same small business feel!
+			</p>
+		</div>
+	</section>
+	<section class="section-3">
+		<div>
+			<h2>Use Amazon in Creative Ways</h2>
+			<p>
+				Leverage Amazon to sell you hand made goods, publish books, and partner with other
+				businesses to create unique product offerings.
+			</p>
+		</div>
+		<img src="/images/creative-img.png" alt="a collection of custom created books." />
+	</section>
 </body>
 
 <style>
@@ -54,7 +48,12 @@
 
 	/* Define a gradient background for a specific element */
 	.gradient-background {
-		background-image: radial-gradient(at 0% 100%,rgba(var(--color-secondary-500) / .5) 0px,transparent 50%), radial-gradient(at 98% 1%,rgba(var(--color-secondary-500) / .33) 0px,transparent 50%);
+		background-image: radial-gradient(
+				at 0% 100%,
+				rgba(var(--color-secondary-500) / 0.5) 0px,
+				transparent 50%
+			),
+			radial-gradient(at 98% 1%, rgba(var(--color-secondary-500) / 0.33) 0px, transparent 50%);
 		/* background: linear-gradient(to bottom, #6376a3, #a48e5b, #6376a3); */
 		/* You can adjust the color values and gradient direction as needed */
 	}
