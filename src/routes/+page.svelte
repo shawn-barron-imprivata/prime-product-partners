8bd8f443fb3770b488a16770489c2d6128605e44
shawnbarron16 <shawnbarron16@gmail.com>
2023-08-10 11:39:05 -0500

    Rough layout of all elements on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <body class="gradient-background">
-	<section class="primer">
+	<section class="primer text-center">
 		<div>
-			<h1>Elevate Your Brand, Expand Your Reach</h1>
-			<p>
+			<h1 class="text text-6xl">Elevate Your Brand, Expand Your Reach</h1>
+			<p class="text text-2xl justify-center">
 				At Prime Product Partners, we provide the most creative strategies revolving around your
 				Amazon store creation. We pride ourselves on working exclusively with small and local
 				businesses. If you run a business or even just hand-make your own art or custom items,
@@ -10,39 +10,58 @@
 			</p>
 		</div>
 	</section>
-	<section class="section-1">
+	<section class="section-1 flex flex-col items-center justify-center text-center p-10 border-t-8 border-secondary-500">
 		<div>
-			<h2>Unlock a New Revenue Stream</h2>
-			<p>
-				Every Business Can Benefit from an Additional Revenue Stream, Especially Small Enterprises!
-			</p>
-		</div>
-		<img src="/images/revenue-img.png" alt="a hand holding a smartphone showing amazon sales." />
-	</section>
-	<section class="section-2">
-		<img src="/images/reach-img.png" alt="graph showing growth in sales." />
-		<div>
-			<h2>Expand Your Reach</h2>
-			<p>
-				Get noticed by thousands of new customers, all while keeping the same small business feel!
-			</p>
+			<h2 class="text text-4xl">Unlock a New Revenue Stream</h2>
+			<div class="flex flex-wrap items-center justify-between mb-6">
+				<p class="w-full md:w-1/2 text text-2xl">
+					Every Business Can Benefit from an Additional Revenue Stream, Especially Small
+					Enterprises!
+				</p>
+				<img
+					src="/images/revenue-img.png"
+					alt="a hand holding a smartphone showing amazon sales."
+					class="w-full md:w-1/2 max-h-500 mb-4 md:mb-0"
+				/>
+			</div>
 		</div>
 	</section>
-	<section class="section-3">
+	<section class="section-2 flex flex-col items-center justify-center text-center p-10 border-t-8 border-secondary-500">
 		<div>
-			<h2>Use Amazon in Creative Ways</h2>
-			<p>
-				Leverage Amazon to sell you hand made goods, publish books, and partner with other
-				businesses to create unique product offerings.
-			</p>
+			<h2 class="text text-4xl">Expand Your Reach</h2>
+			<div class="flex flex-wrap items-center justify-between mb-6">
+				<p class="w-full md:w-1/2 text text-2xl">
+					Get noticed by thousands of new customers, all while keeping the same small business feel!
+				</p>
+				<img
+					src="/images/creative-img.png"
+					alt="a hand holding a smartphone showing amazon sales."
+					class="w-full md:w-1/2 max-h-500 mb-4 md:mb-0"
+				/>
+			</div>
 		</div>
-		<img src="/images/creative-img.png" alt="a collection of custom created books." />
+	</section>
+	<section class="section-3 flex flex-col items-center justify-center text-center p-10 border-t-8 border-secondary-500">
+		<div>
+			<h2 class="text text-4xl">Use Amazon in Creative Ways</h2>
+			<div class="flex flex-wrap items-center justify-between mb-6">
+				<p class="w-full md:w-1/2 text text-2xl">
+					Leverage Amazon to sell you hand made goods, publish books, and partner with other
+				businesses to create unique product offerings
+				</p>
+				<img
+					src="/images/creative-img.png"
+					alt="a hand holding a smartphone showing amazon sales."
+					class="w-full md:w-1/2 max-h-500 mb-4 md:mb-0"
+				/>
+			</div>
+		</div>
 	</section>
 </body>
 
 <style>
 	img {
-		max-width: 1500px;
+		max-width: 500px;
 		max-height: 500px;
 	}
 
@@ -56,5 +75,14 @@
 			radial-gradient(at 98% 1%, rgba(var(--color-secondary-500) / 0.33) 0px, transparent 50%);
 		/* background: linear-gradient(to bottom, #6376a3, #a48e5b, #6376a3); */
 		/* You can adjust the color values and gradient direction as needed */
+	}
+
+	.primer {
+		margin-left: 100px;
+		margin-right: 100px;
+	}
+
+	.section-1 {
+		margin-top: 50px;
 	}
 </style>
