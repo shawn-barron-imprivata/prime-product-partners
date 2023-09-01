5f94ff0dd7125cc62f9c63d9a4242db42310c5da
shawnbarron16 <shawnbarron16@gmail.com>
2023-09-01 15:49:33 -0500

    More serivce page layout adjustments

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,81 +1,111 @@
 <body class="body">
-    <div class="primer">
-        <h1 class="text text-4xl text-center">Our Services</h1>
-        <p class="primer-description text text-2xl text-center">At Prime Product Partners, we provide the most creative strategies revolving around your Amazon store creation. 
-        We pride ourselves on working exclusively with small and local businesses. If you run a business or even just hand-make your own art or custom items, 
-        there's a place for you with us!</p>
-    </div>
-    <section class="product-partnering flex flex-col items-center justify-center">
-        <div class="account-creation flex flex-col items-center justify-center">
-            <div>
-                <h1>Account Creation</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-            <img
-                src="/images/reach-img.png"
-                alt="amazon account creation"
-                class="w-full md:w-1/2 max-h-500 mb-4 mb:mb-0"
-            />
-        </div>
-    </section>
-    <section class="product-images flex flex-col items-center justify-center">
-        <div class="account-creation">
-            <img
-                src="/images/reach-img.png"
-                alt="amazon account creation"
-                class="w-full md:w-1/2 max-h-500 mb-4 mb:mb-0"
-            />
-            <div>
-                <h1>Product Images</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-        </div>
-    </section>
-    <section class="keyword-resesarch flex flex-col items-center justify-center">
-        <div class="account-creation">
-            <div>
-                <h1>Keyword research</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-            <img
-                src="/images/reach-img.png"
-                alt="amazon account creation"
-                class="w-full md:w-1/2 max-h-500 mb-4 mb:mb-0"
-            />
-        </div>
-    </section>
-    <section class="product-images flex flex-col items-center justify-center">
-        <div class="account-growth">
-            <img
-                src="/images/reach-img.png"
-                alt="amazon account creation"
-                class="w-full md:w-1/2 max-h-500 mb-4 mb:mb-0"
-            />
-            <div>
-                <h1>Account Growth</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-        </div>
-    </section>
+	<div class="primer">
+		<h1 class="text text-4xl text-center">Our Services</h1>
+		<h2 class="primer-description text text-2xl text-center">
+			At Prime Product Partners, we provide the most creative strategies revolving around your
+			Amazon store creation. We pride ourselves on working exclusively with small and local
+			businesses. If you run a business or even just hand-make your own art or custom items, there's
+			a place for you with us!
+		</h2>
+	</div>
+	<!-- Create 4 different sections highlighting the services offered -->
+	<section
+		class="account-creation flex flex-row items-center justify-center p-10 border-t-8 border-secondary-500"
+	>
+		<div class="text-center">
+			<h1 class="service-title text text-4xl">Account Creation</h1>
+			<p class="text text-2xl">
+				Your Amazon Success Starts Here!
+                Comprehensive Guidance to Launch and Register Your Brand on
+				the Amazon Marketplace. 
+			</p>
+		</div>
+		<img
+			src="/images/reach-img.png"
+			alt="amazon account creation"
+			class="w-full md:w-1/2 max-h-500 mb-4 mb:mb-0"
+		/>
+	</section>
+	<section
+		class="product-images flex flex-row items-center justify-center p-10 border-t-8 border-secondary-500"
+	>
+		<img
+			src="/images/reach-img.png"
+			alt="amazon account creation"
+			class="w-full md:w-1/2 max-h-500 mb-4 mb:mb-0"
+		/>
+		<div class="text-center">
+			<h1 class="service-title text text-3xl">Product Images</h1>
+			<p class="text text-2xl">
+				Impress and Attract!
+                Crafting Captivating Visuals to Win Over New Buyers.
+			</p>
+		</div>
+	</section>
+	<section
+		class="keyword-resesarch flex flex-row items-center justify-center p-10 border-t-8 border-secondary-500"
+	>
+		<div class="text-center">
+			<h1 class="service-title text text-3xl">Keyword research</h1>
+			<p class="text text-2xl">
+				Unlocking Visibility and Conversion!
+                Harnessing Relevant Keywords for Engaging Titles and Descriptions.
+			</p>
+		</div>
+		<img
+			src="/images/reach-img.png"
+			alt="amazon account creation"
+			class="w-full md:w-1/2 max-h-500 mb-4 mb:mb-0"
+		/>
+	</section>
+	<section
+		class="product-images flex flex-row items-center justify-center p-10 border-t-8 border-secondary-500"
+	>
+		<img
+			src="/images/reach-img.png"
+			alt="amazon account creation"
+			class="w-full md:w-1/2 max-h-500 mb-4 mb:mb-0"
+		/>
+		<div class="text-center">
+			<h1 class="service-title text text-3xl">Watch Your Account Skyrocket</h1>
+			<p class="text text-2xl">
+				Seamless Support for End-to-End Amazon Success!
+                The Most Streamlined Approach to Maximizing the Potential of Amazon.
+			</p>
+		</div>
+	</section>
 </body>
 
 <style>
-    .body {
-        background: linear-gradient(to bottom, #6376a3, #a48e5b, #6376a3);
-    }
+	.body {
+		background: linear-gradient(to bottom, #6376a3, #a48e5b, #6376a3);
+	}
 
-    .primer {
-        margin-left: 30ex;
+	.primer {
+		margin-left: 30ex;
 		margin-right: 30ex;
-		margin-top: 20px
-    }
+		margin-top: 20px;
+	}
 
-    .primer-description {
-        margin-top: 3ex;
-    }
+	.primer-description {
+		margin-top: 3ex;
+	}
 
-    img {
+	section {
+		margin-top: 50px;
+	}
+
+	img {
 		max-width: 500px;
 		max-height: 500px;
+        margin-left: 10ex;
+        margin-right: 10ex;
+	}
+
+	p {
+		margin-right: 10ex;
+		margin-left: 10ex;
+		margin-top: 2ex;
+		max-width: 300px;
 	}
 </style>
